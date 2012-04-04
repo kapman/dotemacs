8a42374d11965b65c1ab8aae3ecc02ed7929cdab
Aamir Kapasi <aamirkapasi89@gmail.com>
2012-04-04 15:53:11 +0530

    Remove whitespace fix in magit

--- config/magit.el
+++ config/magit.el
@@ -9,4 +9,7 @@
      (set-face-foreground 'magit-diff-del "red3")
      (set-face-background 'magit-item-highlight "gray15")
      (set-face-foreground 'magit-diff-hunk-header "orange")
-     (set-face-foreground 'magit-branch "gold")))
+     (set-face-foreground 'magit-branch "gold")
+     (setq require-final-newline t)
+     (add-hook 'before-save-hook 'delete-trailing-whitespace)
+     (setq-default indent-tabs-mode nil)))
