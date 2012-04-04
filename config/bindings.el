9b6b7d8f458c8c8916220e98b78538f98479f9da
Aamir Kapasi <aamirkapasi89@gmail.com>
2012-04-04 15:40:35 +0530

    Some keyboard shortcuts from starter-kit

--- config/bindings.el
+++ config/bindings.el
@@ -9,6 +9,7 @@
 (global-set-key (kbd "C-c T") 'multi-term) ;; create a new one
 
 (global-set-key (kbd "<f5>") 'revert-all-buffers)
+(global-set-key (kbd "C-c r") 'revert-buffer)
 (global-set-key (kbd "<f11>") 'full-screen-toggle)
 
 (global-set-key "\C-xk" 'kill-current-buffer)
@@ -17,11 +18,14 @@
 (global-set-key (kbd "C-x r") 'rgrep)
 (global-set-key (kbd "M-g") 'goto-line)
 
-;; (global-set-key (kbd "<up>") 'punish-me)
-;; (global-set-key (kbd "<down>") 'punish-me)
-;; (global-set-key (kbd "<left>") 'punish-me)
-;; (global-set-key (kbd "<right>") 'punish-me)
+;; Window switching. (C-x o goes to the next window)
+(windmove-default-keybindings) ;; Shift+direction
+(global-set-key (kbd "C-x O") (lambda () (interactive) (other-window -1))) ;; back one
+(global-set-key (kbd "C-x C-o") (lambda () (interactive) (other-window 2))) ;; forward two
 
+;; Font size
+(define-key global-map (kbd "C-+") 'text-scale-increase)
+(define-key global-map (kbd "C--") 'text-scale-decrease)
 
 (eval-after-load "magit"
   (progn '(global-set-key (kbd "C-x g") 'magit-status)))
