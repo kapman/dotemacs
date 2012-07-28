97854e58714ab129a305fb44bec150cd36085397
Aamir Kapasi <aamir@helpshift.com>
2012-07-28 19:00:48 +0530

    Some keyboard shortcuts from starter kit

--- packs/user/init.el
+++ packs/user/init.el
@@ -3,3 +3,9 @@
 
 ;; Use M-return to switch between windowed and full-screen modes
 (global-set-key (kbd "M-RET") 'ns-toggle-fullscreen)
+
+(global-set-key (kbd "C-c r") 'revert-buffer)
+
+(windmove-default-keybindings) ;; Shift+direction
+(global-set-key (kbd "C-x O") (lambda () (interactive) (other-window -1))) ;; back one
+(global-set-key (kbd "C-x C-o") (lambda () (interactive) (other-window 2))) ;; forward two
