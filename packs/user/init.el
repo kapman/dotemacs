148a50a3658b77ab1ba605afa3148e0c05fbd128
Aamir Kapasi <aamir@helpshift.com>
2012-07-28 17:46:54 +0530

    Add keybinding for Mac full-screen

--- packs/user/init.el
+++ packs/user/init.el
@@ -1,2 +1,5 @@
 (setq user-full-name "Aamir Kapasi")
 (setq user-mail-address "aamirkapasi89@gmail.com")
+
+;; Use M-return to switch between windowed and full-screen modes
+(global-set-key (kbd "M-RET") 'ns-toggle-fullscreen)
