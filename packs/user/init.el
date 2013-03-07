05ef2c094bc6320f001a517cc87ad1cf60850d46
Aamir Kapasi <aamir@helpshift.com>
2013-03-07 13:16:13 +0530

    Revert all buffers key binding

--- packs/user/init.el
+++ packs/user/init.el
@@ -8,6 +8,7 @@
 (global-set-key (kbd "M-j") 'pop-to-mark-command)
 
 (global-set-key (kbd "C-c r") 'revert-buffer)
+(global-set-key (kbd "C-c C-r") 'revert-all-buffers)
 
 (windmove-default-keybindings) ;; Shift+direction
 (global-set-key (kbd "C-x O") (lambda () (interactive) (other-window -1))) ;; back one
