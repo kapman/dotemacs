bb68132e79f152a25bc93a3fdd30d9a40809ad28
Aamir Kapasi <aamir@helpshift.com>
2013-09-15 16:09:34 +0530

    Added tagedit pack
    - Added s.el and dash.el as dependencies

---
+++ packs/programming/config/html-conf.el
@@ -0,0 +1,9 @@
+(require 'tagedit)
+
+(eval-after-load "sgml-mode"
+  '(progn
+     (require 'tagedit)
+     (tagedit-add-paredit-like-keybindings)
+     (add-hook 'html-mode-hook (lambda () (tagedit-mode 1)))))
+
+(tagedit-add-experimental-features)
