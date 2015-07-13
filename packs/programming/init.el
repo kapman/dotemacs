31604c05c17577a754e8c5a3f2cd9da668689839
Aamir Kapasi <aamir@helpshift.com>
2015-07-13 17:15:07 +0530

    Comment out unused languages

--- packs/programming/init.el
+++ packs/programming/init.el
@@ -3,10 +3,10 @@
 (require 'mustache-mode)
 
 (live-load-config-file "python-conf.el")
-(live-load-config-file "haskell-conf.el")
+;; (live-load-config-file "haskell-conf.el")
 (live-load-config-file "scss-conf.el")
 (live-load-config-file "markdown-conf.el")
-(live-load-config-file "golang-conf.el")
+;; (live-load-config-file "golang-conf.el")
 (live-load-config-file "js-conf.el")
 (live-load-config-file "zencoding-conf.el")
 (live-load-config-file "html-conf.el")
